fe6538d39ec4af4dc33d644fe9e82638f6dfbae5
Irvin Do <irvindo@yahoo.com>
2018-01-28 20:58:16 -0500

    Final Project

---
+++ src/mediaResponses.js
@@ -0,0 +1,80 @@
+const fs = require('fs'); // pull in file system module
+const path = require('path');
+
+function loadFile(request, response, url) {
+  const file = path.resolve(__dirname, url);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
+
+    let { range } = request.headers;
+
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = end - 1;
+    }
+
+    const chunksize = (end - start) + 1;
+
+    const contentParse = url.split('.');
+    const mediaType = contentParse[contentParse.length - 1];
+    // console.log(contentType[contentType.length-1]);
+    let contentType = '';
+
+    if (mediaType === 'mp4') {
+      contentType = 'video/mp4';
+    } else if (mediaType === 'mp3') {
+      contentType = 'audio/mpeg';
+    }
+
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': `${contentType}`,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
+}
+
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4');
+};
+
+const getBlind = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4');
+};
+
+module.exports.getParty = getParty;
+module.exports.getBlind = getBlind;
+module.exports.getBird = getBird;
